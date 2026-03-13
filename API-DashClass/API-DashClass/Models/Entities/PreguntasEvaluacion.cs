ca62a6920372786896e8c289bf33d9653c471316
DualyRF <dualyrf@gmail.com>
2026-03-13 00:42:54 -0700

    Mapeo de ultimas tablas

---
+++ API-DashClass/API-DashClass/Models/Entities/PreguntasEvaluacion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace API_DashClass.Models.Entities
+{
+    [Table("preguntas_evalucion")]
+    id_pregunta INT PRIMARY KEY AUTO_INCREMENT,
+    id_evaluacion INT NOT NULL,
+    texto_pregunta TEXT NOT NULL,
+    url_imagen VARCHAR(500),
+    tipo ENUM('OpcionMultiple', 'VerdaderoFalso', 'RespuestaCorta') NOT NULL,
+    opciones JSON,
+    respuestas_correctas JSON,
+    tiempo_limite INT,
+    orden INT NOT NULL,
+    en_banco_preguntas BOOLEAN DEFAULT FALSE,
+    public class PreguntasEvaluacion
+    {
+        public enum TipoPregunta
+        {
+            OpcionMultiple,
+            VerdaderoFalso,
+            RespuestaCorta
+        }
+        [Key]
+        [Column("id_pregunta")]
+        public int IdPregunta { get; set; }
+
+        [Column("id_evaluacion")]
+        [Required]
+        public int IdEvaluacion { get; set; }
+
+        [Column("texto_pregunta")]
+        [Required]
+        public string TextoPregunta { get; set; }
+
+        [Column("url_imagen")]
+        [MaxLength(500)]
+        public string? UrlImagen { get; set; }
+
+        [Column("tipo")]
+        [Required]
+        public TipoPregunta Tipo { get; set; }
+
+        [Column("opciones")]
+        public string? Opciones { get; set; }
+
+        [Column("respuestas_correctas")]
+        public string? RespuestasCorrectas { get; set; }
+
+        [Column("tiempo_limite")]
+        public int? TiempoLimite { get; set; }
+
+        [Column("orden")]
+        [Required]
+        public int Orden { get; set; }
+
+        [Column("en_banco_preguntas")]
+        public bool EnBancoPreguntas { get; set; } = false;
+    }
+}
---
+++ API-DashClass/API-DashClass/Models/Entities/TransferenciasPuntos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace API_DashClass.Models.Entities
+{
+    [Table("transferencias_puntos")]
+    public class TransferenciasPuntos
+    {
+        [Key]
+        [Column("id_transferencia")]
+        public int IdTransferencia { get; set; }
+
+        [Column("desde_id_usuario")]
+        [Required]
+        public int DesdeIdUsuario { get; set; }
+
+        [Column("hacia_id_usuario")]
+        [Required]
+        public int DesdeDesde { get; set; }
+
+        [Column("id_curso")]
+        [Required]
+        public int IdCurso { get; set; }
+
+        [Column("cantidad")]
+        [Required]
+        public int Cantidad { get; set; }
+
+        [Column("mensaje")]
+        public string? Mensaje { get; set; }
+
+        [Column("anonima")]
+        public bool Anonima { get; set; } = false;
+
+        [Column("codigo_transferencia")]
+        [Required]
+        [MaxLength(20)]
+        public string CodigoTransferencia { get; set; }
+
+        [Column("fecha_transferencia")]
+        public DateTime FechaTransferencia { get; set; } = DateTime.Now;
+    }
+}
