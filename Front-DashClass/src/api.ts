7e99afd5c8c0954fad9c6ff4ce5e78052da48754
Diego Espiritu <diegoesp1011@gmail.com>
2026-03-16 15:17:53 -0700

    Inicio de Sesion con google funcionando - Claves sin mostrar

--- Front-DashClass/src/api.ts
+++ Front-DashClass/src/api.ts
@@ -3,4 +3,5 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export const API = {
   register: `${BASE_URL}/api/Auth/register`,
   login: `${BASE_URL}/api/Auth/login`,
+  googleLogin: `${BASE_URL}/api/Auth/google`,
 };
